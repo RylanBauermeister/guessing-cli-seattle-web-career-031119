db99946faf2b6a03434ee921a6cd479df2bd16d3
Rylan Bauermeister <rylan@bauermeister.com>
2019-02-18 01:39:07 +0000

    Automatically backed up by Learn

--- guessing_cli.rb
+++ guessing_cli.rb
@@ -3,14 +3,14 @@ def run_guessing_game
   puts "Guess a number between 1 and 6."
   loop do 
     target = gets.chomp
-    guess =   target = rand(1...7)
+    guess = rand(1...7)
     if guess == target.to_i
       puts "/You guessed the correct number!/"
     elsif guess == 'exit'
       exit
       break
     else
-      puts "/The computer guessed #{guess.to_i}./"
+      puts "/The computer guessed #{guess}./"
     end
   end
 end
